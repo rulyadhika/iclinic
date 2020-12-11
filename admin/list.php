54bb40032ca7e7324e529b7da7d2893f23786cec
rulyadhika <rulihanif@gmail.com>
2020-12-12 02:02:58 +0700

    menambahkan footer pada halaman dashboard admin

--- admin/list.php
+++ admin/list.php
@@ -175,15 +175,7 @@ $no = 1;
   </aside>
   <!-- /.control-sidebar -->
 
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+  <?php require '../components/admin/footer.php' ?>
 </div>
 <!-- ./wrapper -->
 
---
+++ components/admin/footer.php
@@ -0,0 +1,17 @@
+<?php 
+
+//handler jika file ini diakses langsung dari url
+if(!defined('root')){
+  header('location:'.BASEURL);die;
+}
+
+?>
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Made With <i class="fa fa-heart"></i> by Mahasiswa IF Unsoed
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020-2021 <a href="javascript:void(0)">I-Clinic Unsoed</a>.</strong> All rights reserved.
+</footer>
